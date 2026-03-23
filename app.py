a0eed852cc9be8263bfd873680830f6cbad30841
BushraShaikh4704 <bushrashaikh4704@gmail.com>
2026-03-23 17:03:30 +0530

    Added dataset and cleaned CSV

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+import pickle
+
+# Load model
+with open('/content/drive/MyDrive/Fake_Job_Project/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('/content/drive/MyDrive/Fake_Job_Project/vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+st.title("Fake Job Posting Detection")
+
+user_input = st.text_area("Enter Job Description")
+
+if st.button("Predict"):
+    text_vector = vectorizer.transform([user_input])
+    prediction = model.predict(text_vector)[0]
+
+    if prediction == 1:
+        st.error("🚨 Fake Job")
+    else:
+        st.success("✅ Real Job")
